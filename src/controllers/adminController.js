4925957ad087a4917228fde8f1d8ff926c487355
AlenLong <alenlong1@gmail.com>
2022-10-16 16:48:17 -0300

    modificar y actualizar base de datos

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -13,6 +13,9 @@ module.exports = {
                 res.send(err)
             });
     },
+
+    /* ============================ */
+
     store : (req,res) => {
 
         let{title,rating,awards,release_date,length,genre} = req.body
@@ -32,13 +35,60 @@ module.exports = {
             res.send(err)
         })
     },
+
+        /* ============================ */
+
     edit : (req,res) => {
-        res.render('crearPelicula')
+        let idParams = +req.params.id 
+        /* db.Movies.findByPk(idParams) */ /* otro metodo para traer una sola pelicula */
+        let generos = db.Genre.findAll()
+        let pelicula = db.Movies.findOne({
+            where : {
+                id : idParams
+            }
+        })
+        Promise.all([generos,pelicula])
+        .then(([generos,pelicula]) => {
+            return res.render('editarPelicula',{
+                generos,
+                pelicula
+            })
+            res.render('editarPelicula')
+        }).catch((err) => {
+            res.send(err)
+        });
     },
+
+        /* ============================ */
+
     update : (req,res) => {
-        res.redirect('/')
+        let idParams = +req.params.id
+        let{title,rating,awards,release_date,length,genre} = req.body
+        db.Movies.update({
+            title: title,
+            rating: +rating,
+            awards: +awards,
+            release_date: Date(release_date),
+            length: +length,
+            genre_id: +genre,
+            updated_at: new Date
+        },{
+            where : {
+                id : idParams
+            }
+        })
+        .then((result) => {
+            return res.redirect('/admin/create')
+        }).catch((err) =>{
+            res.send(err)
+        })
+        res.redirect('/admin/create')
     },
+
+        /* ============================ */
+
     destroy : (req,res) => {
+        
         res.redirect('crearPelicula')
     }
 }
